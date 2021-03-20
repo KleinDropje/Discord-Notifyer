c9039e201e88f44bece3a26bfc2b923cac14719e
KleinDropje <76535252+KleinDropje@users.noreply.github.com>
2021-03-20 17:44:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+const Discord = require("discord.js");
+
+const bot = new Discord.Client();
+
+const ping = require("minecraft-server-util");
+
+const token = "ODIyNTQ4MDQ1MzM2OTM2NDQ5.YFT3kA.qWpOatMLNseL4DlEVM9ULqtBgTY";
+
+const prefix = "!";
+
+bot.on("ready", () => {
+  console.log("Bot has come online.");
+});
+
+bot.on("message", (message) => {
+  let args = message.content.split(" ");
+
+
+  if (message.content.startsWith(prefix + "N help")) {
+    const Embed = new Discord.MessageEmbed()
+    .setColor("#C86A56")
+    .setTitle("N create")
+    .setAuthor("Notifyer Help")
+    .setDescription("Create a notification, after that you'll be send a dm about your notification")
+    .setTimestamp()
+    .setFooter("Notifyer");
+
+    message.channel.send(Embed);
+
+  }
+  //Create Memory
+  if (message.content.startsWith(prefix + "N create")) {
+    message.channel
+      .send(`What would you like to call your appointment ?`)
+      .then(() => {
+        message.channel
+          .awaitMessages((m) => m.author.id == message.author.id, {
+            max: 1,
+            time: 20000,
+          })
+          .then((message) => {
+            message = message.first();
+            const Name = message.content;
+            message.channel
+              .send(`When would you like us to notify you (in minutes)?`)
+              .then(() => {
+                message.channel
+                  .awaitMessages((m) => m.author.id == message.author.id, {
+                    max: 1,
+                    time: 20000,
+                  })
+                  .then((message) => {
+                    message = message.first();
+                    const Time = message.content;
+                    if (!Number(message.content)) {
+                      message.channel.send("Please insert a number.");
+                      return;
+                    }
+                    const Embed = new Discord.MessageEmbed()
+                      .setColor("#C86A56")
+                      .setTitle(Name)
+                      .setAuthor("You have a new notification!")
+                      .setDescription(`<@${message.author.id}>`)
+                      .setTimestamp()
+                      .setFooter("Notifyer");
+
+                      message.channel.send("Your Notification has been made...");
+                      setTimeout(() => {
+                        message.author.send(Embed);
+                        }, Time * 1000 * 60);
+                  })
+                  .catch((collected) => {
+                    message.channel.send(
+                      `After 20 seconds nobody reacted, stopping...`
+                    );
+                  });
+              });
+          })
+          .catch((collected) => {
+            message.channel.send(
+              `After 20 seconds nobody reacted, stopping...`
+            );
+          });
+      });
+  }
+});
+
+bot.login(token);
